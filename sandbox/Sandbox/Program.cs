a1b4d3a90f7b9430576c15e698db994f58bc7d8a
WillFlet09 <purrkinssaysno@gmail.com>
2025-02-25 09:50:08 -0700

    The Bridge of Death

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,5 +6,14 @@ class Program
     {
         Console.WriteLine("Hello Sandbox World!");
         Console.WriteLine("Hello CSE 210");
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}");
+        Console.WriteLine("What is your quest?");
+        string quest = Console.ReadLine();
+        Console.WriteLine($"{quest}");
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"{color}");
     }
 }
