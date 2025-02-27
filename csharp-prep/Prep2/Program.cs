43edd73d1d4b7b46c49bc6bcb75bb1f7410bf5df
WillFlet09 <purrkinssaysno@gmail.com>
2025-02-27 09:57:47 -0700

    Grade A coding

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,86 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Grade? ");
+        string userInput = Console.ReadLine();
+        int x = int.Parse(userInput);
+        string Grade = "U";
+
+        if (x >= 93) 
+        {
+            Grade = "A";
+        }
+
+        if (x >= 90 && x < 93)
+        {
+            Grade = "A-";
+        } 
+
+        if (x >= 87 && x < 90)
+        {
+            Grade = "B+";
+        } 
+
+        if (x >= 83 && x < 87)
+        {
+            Grade = "B";
+        }
+
+        if (x >= 80 && x < 83)
+        {
+            Grade = "B-";
+        }
+
+        if (x >= 77 && x < 80)
+        {
+            Grade = "C+";
+        }
+
+        if (x >= 73 && x < 77)
+        {
+            Grade = "C";
+        }
+
+        if (x >= 70 && x <73)
+        {
+            Grade = "C-";
+        }
+
+        if (x >= 67 && x < 70)
+        {
+            Grade = "D+";
+        }
+
+        if (x >= 63 && x < 67)
+        {
+            Grade = "D";
+        }
+
+        if (x >= 60 && x < 63)
+        {
+            Grade = "D-";
+        }
+
+        if (x >= 63 && x < 60)
+        {
+            Grade = "F";
+        }
+
+        if (x < 63)
+        {
+            Grade = "F-";
+        }
+
+        Console.WriteLine($"You scored {x} so you get a(n) {Grade}");
+
+        if(x >= 70)
+        {
+            Console.WriteLine("You Passed the Class!");
+        }
+        else
+        {
+            Console.WriteLine("You Failed!");
+        }
+
     }
 }
